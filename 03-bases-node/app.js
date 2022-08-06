389e0af48474977c120c2549ac0e5e1bf415caa1
Luis Chilo <gfxargentina@gmail.com>
2022-08-05 21:09:26 -0300

    31- refactor - promesas

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,22 +1,10 @@
-const fs = require('fs');
+const { crearArchivo } = require('./helpers/multiplicar');
 
 //borra la consola
 console.clear();
 
-const base = 3;
-let salida = '';
+const base = 6;
 
-console.log('======================');
-console.log(`Tabla del ${base}`);
-console.log('======================');
-
-// alt+92: barra invertida \, \n: salto de linea
-for (let i = 1; i <= 10; i++) {
-  salida += `${base} x ${i} = ${base * i}\n`;
-}
-
-fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
-  if (err) throw err;
-
-  console.log(`tabla-${base}.txt creada`);
-});
+crearArchivo(base)
+  .then((nombreArchivo) => console.log(nombreArchivo, 'creado'))
+  .catch((err) => console.log(err));
---
+++ 03-bases-node/helpers/multiplicar.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+//cuando le pones el async a una funcion, por defecto ya esta regresando una promesa
+//y se maneja con el try and catch
+const crearArchivo = async (base) => {
+  try {
+    console.log('======================');
+    console.log('Tabla del', base);
+    console.log('======================');
+
+    let salida = '';
+
+    // alt+92: barra invertida \, \n: salto de linea
+    for (let i = 1; i <= 10; i++) {
+      salida += `${base} x ${i} = ${base * i}\n`;
+    }
+
+    fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
+      if (err) throw err;
+
+      return `tabla-${base}.txt`;
+    });
+  } catch (error) {
+    throw error;
+  }
+
+  //una de las formas de convertir la funcion en promesa
+  //   return new Promise((resolve, reject) => {
+  //     console.log('======================');
+  //     console.log('Tabla del', base);
+  //     console.log('======================');
+
+  //     let salida = '';
+
+  //     // alt+92: barra invertida \, \n: salto de linea
+  //     for (let i = 1; i <= 10; i++) {
+  //       salida += `${base} x ${i} = ${base * i}\n`;
+  //     }
+
+  //     fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
+  //       if (err) throw err;
+
+  //       resolve(`tabla-${base}.txt creada`);
+  //     });
+  //   });
+};
+
+module.exports = { crearArchivo };
