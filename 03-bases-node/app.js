1fcdb08959cccff279312647b8a192397dd1d3ca
Luis Chilo <gfxargentina@gmail.com>
2022-08-07 12:15:10 -0300

    36 configuraciones yargs independiente

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,23 +1,5 @@
-const { options } = require('yargs');
+const argv = require('./config/yargs');
 const { crearArchivo } = require('./helpers/multiplicar');
-const argv = require('yargs')
-  .option('b', {
-    alias: 'base',
-    type: 'number',
-    demandOption: true,
-  })
-  .option('l', {
-    alias: 'listar',
-    type: 'boolean',
-    demandOption: true,
-    default: false,
-  })
-  .check((argv, options) => {
-    if (isNaN(argv.b)) {
-      throw 'La base tiene que ser un numero';
-    }
-    return true;
-  }).argv;
 
 //borra la consola
 console.clear();
