cf357fab32fd40a4881d63d81afc902d76941f0b
Luis Chilo <gfxargentina@gmail.com>
2022-11-18 11:50:26 -0300

    add: user inputs validation

--- 07-rest-server/routes/user.routes.js
+++ 07-rest-server/routes/user.routes.js
@@ -5,12 +5,26 @@ const {
   updateUsuario,
   deleteUsuario,
 } = require('../controllers/usuarios.controller');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 router.get('/', getUsuarios);
 
-router.post('/', usuariosPost);
+router.post(
+  '/',
+  [
+    check('correo', 'El correo no es valido').isEmail(),
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password debe de ser de mas de 6 letras').isLength({
+      min: 6,
+    }),
+    check('rol', 'No es un Rol valido').isIn(['ADMIN', 'USER']),
+    validarCampos,
+  ],
+  usuariosPost
+);
 
 router.put('/:id', updateUsuario);
 
